4ff9f020037b479da76cdc847d45339a54806913
ozziepeeps <mark@markosborn.me>
2014-10-24 15:37:56 -0700

    More unit tests.

--- PodcastWP.Tests/PodcastHelperTests.cs
+++ PodcastWP.Tests/PodcastHelperTests.cs
@@ -87,5 +87,41 @@
 
             this.launcher.VerifyAll();
         }
+
+        [TestMethod]
+        public void SubscribeQueryString()
+        {
+            var expectedUri = new Uri("wp-podcast://Subscribe/?feedUrl=http%3A%2F%2Ftwit.tv%2Fww%3Fepisode%3D1");
+
+            this.launcher.Setup(s => s.LaunchUriAsync(expectedUri)).Verifiable();
+
+            PodcastHelper.SubscribeToPodcast(new Uri("http://twit.tv/ww?episode=1"));
+
+            this.launcher.VerifyAll();
+        }
+
+        [TestMethod]
+        public void SubscribeUserName()
+        {
+            var expectedUri = new Uri("wp-podcast://Subscribe/?feedUrl=http%3A%2F%2Fusername%40twit.tv%2Fww");
+
+            this.launcher.Setup(s => s.LaunchUriAsync(expectedUri)).Verifiable();
+
+            PodcastHelper.SubscribeToPodcast(new Uri("http://username@twit.tv/ww"));
+
+            this.launcher.VerifyAll();
+        }
+
+        [TestMethod]
+        public void SubscribePassword()
+        {
+            var expectedUri = new Uri("wp-podcast://Subscribe/?feedUrl=http%3A%2F%2Fusername%3Apassword%40twit.tv%2Fww");
+
+            this.launcher.Setup(s => s.LaunchUriAsync(expectedUri)).Verifiable();
+
+            PodcastHelper.SubscribeToPodcast(new Uri("http://username:password@twit.tv/ww"));
+
+            this.launcher.VerifyAll();
+        }
     }
 }
